38eaf2b44cc0266fb9adf275464ddeb0d9bf1d30
yamam00s <s.yamamo0327@gmail.com>
2023-03-17 15:43:09 +0900

    Chatが以前の会話を維持

--- src/app/chat/components/Chat.tsx
+++ src/app/chat/components/Chat.tsx
@@ -3,28 +3,53 @@
 import { useState, useCallback, ChangeEvent, FormEvent } from 'react'
 import { fetchOpenai, FetchOpenaiResponse } from '../api/fetchOpenai'
 
+type Role = 'user' | 'gpt'
+type Message = {
+  id: number,
+  role: Role,
+  text: FetchOpenaiResponse
+}
+
 export const Chat = () => {
-  const [response, setResponse] = useState('')
-  const [message, setMessages] = useState('')
+  const [prompt, setPrompt] = useState('')
+  const [messages, setMessages] = useState<Message[]>([])
+  const [messageId, setMessageId] = useState(0)
+
+  const addMessage = useCallback((role: Role, text: string) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { id: messageId, role, text}
+    ]);
+    setMessageId((prevCounter) => prevCounter + 1)
+  }, [messageId, setMessages])
+
+  const handleTextareaChange = useCallback((event: ChangeEvent<HTMLTextAreaElement>) => {
+    setPrompt(event.target.value)
+  }, [setPrompt])
 
   const handleSubmit = useCallback(async (event: FormEvent) => {
     event.preventDefault()
+    addMessage('user', prompt)
     // const response: FetchOpenaiResponse = await fetchOpenai(message)
     const response: FetchOpenaiResponse = 'hello'
-    setResponse(response)
-  }, [message, setResponse])
-
-  const handleTextareaChange = useCallback((event: ChangeEvent<HTMLTextAreaElement>) => {
-    setMessages(event.target.value)
-  }, [setMessages])
+    addMessage('gpt', response)
+    setPrompt('')
+  }, [prompt, addMessage])
 
   return (
-    <div>
+    <div className='chat'>
       <form onSubmit={handleSubmit}>
-        <textarea value={message} onChange={handleTextareaChange} />
+        <textarea value={prompt} onChange={handleTextareaChange} />
         <button type='submit'>submit</button>
       </form>
-      {response && <p>{response}</p>}
+
+      <div className='messages'>
+        {messages.map(message => (
+          <p key={message.id}>
+            {message.role}: {message.text}
+          </p>
+        ))}
+      </div>
     </div>
   )
 }
