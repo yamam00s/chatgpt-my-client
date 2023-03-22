1a1b2a1bbd402d2bca5768dec2f0edd065b4d7b0
yamam00s <s.yamamo0327@gmail.com>
2023-03-22 22:00:53 +0900

    components/Buttonのstory作成

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryFn, Meta } from '@storybook/react'
+import { Button, ButtonProps } from './Button'
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as Meta<ButtonProps>;
+
+const Template: StoryFn<ButtonProps> = (args) => <Button {...args}/>;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: 'Primary Button',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: 'Primary Button',
+  disabled: true
+};
