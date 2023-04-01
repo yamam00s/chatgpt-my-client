9fabd4ceaccb09a2d246dcb0567e7818a47b2aa6
yamam00s <s.yamamo0327@gmail.com>
2023-04-01 23:41:10 +0900

    retrieveChatGPTResponse mockを使用したUT作成 エラー時のパターン

--- src/app/chat/api/__tests__/retrieveChatGPTResponse.spec.ts
+++ src/app/chat/api/__tests__/retrieveChatGPTResponse.spec.ts
@@ -1,7 +1,12 @@
+import { rest } from 'msw';
+
+import { OPENAI_API_URL } from '@/constants';
 import { mockResponse } from '@/mocks/handlers';
 import { server } from '@/mocks/server';
 
 import { retrieveChatGPTResponse } from '../retrieveChatGPTResponse';
+
+const testPrompt = 'test';
 
 describe('retrieveChatGPTResponse', () => {
   beforeAll(() => {
@@ -17,9 +22,19 @@ describe('retrieveChatGPTResponse', () => {
   });
 
   it('正しいレスポンスが返却されているか', async () => {
-    const testPrompt = 'test';
     const response = await retrieveChatGPTResponse(testPrompt);
 
     expect(response).toEqual(mockResponse.choices[0].message.content);
+  });
+
+  it('エラー時に正しいレスポンスが返却されているか', async () => {
+    server.use(
+      rest.post(OPENAI_API_URL, (req, res, ctx) => {
+        return res.once(ctx.status(500), ctx.json({ message: 'エラーが発生しました' }));
+      })
+    );
+
+    const response = await retrieveChatGPTResponse(testPrompt);
+    expect(response).toEqual('エラーが発生しました');
   });
 });
